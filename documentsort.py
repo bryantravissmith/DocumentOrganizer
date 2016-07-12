74546a5b4a23de858d6eacd318b772973e0df41c
Bryan Travis Smith <bryantravissmith@gmail.com>
2016-07-12 14:10:00 +0000

    added tesseract functions - not using

--- documentsort.py
+++ documentsort.py
@@ -7,13 +7,20 @@ import cv2
 from wand.image import Image
 from wand.color import Color
 import shutil
+import subprocess
+
+"""
+command = ["tesseract","img_0001.jpg","stdout"]
+proc = subprocess.Popen(command,stdout=subprocess.PIPE,stderr=subprocess.STDOUT)
+tiaa_text = proc.stdout.read()
+"""
 
 def convertPdfToImage(file_loc):
 	new_dir = file_loc.split(".")[0]
 	if not os.path.exists(new_dir):
 		os.makedirs(new_dir)
-	doc = Image(filename=file_loc,resolution=200)
-	doc.compression_quality = 80
+	doc = Image(filename=file_loc,resolution=300)
+	doc.compression_quality = 20
 	for i, page in enumerate(doc.sequence):
 		with Image(page) as page_image:
 			page_image.alpha_channel = False
@@ -21,13 +28,25 @@ def convertPdfToImage(file_loc):
 			save_location =  new_dir+"/img-{}.jpg".format(str(i).zfill(4))
 			page_image.save(filename=save_location)
 
+def getImageLocationsForPdf(file_loc):
+	new_dir = file_loc.split(".")[0]
+	return glob.glob(new_dir+"/*.jpg")
 
 def readImagesForPdf(file_loc,img_read_method=cv2.IMREAD_UNCHANGED):
-    new_dir = file_loc.split(".")[0]
-    images = []
-    for f in glob.glob(new_dir+"/*.jpg"):
-        images.append(cv2.imread(f,img_read_method))
-    return images
+	images = []
+	for f in getImageLocationsForPdf(file_loc):
+		images.append(cv2.imread(f,img_read_method))
+	return images
+
+def detectTextInImage(text_list,doc_image_location):
+	command = ["tesseract",doc_image_location,"stdout"]
+	proc = subprocess.Popen(command,stdout=subprocess.PIPE,stderr=subprocess.STDOUT)
+	img_text = proc.stdout.read().lower()
+	print img_text
+	for text in text_list:
+		if text in img_text:
+			return True
+	return False
 
 def detectKeyInImage(key_image,doc_image,max_features=1000000,min_matches=10):
 	
@@ -56,8 +75,8 @@ def detectKeyInImage(key_image,doc_image,max_features=1000000,min_matches=10):
 	# ratio test as per Lowe's paper
 	count = 0
 	for i,(m,n) in enumerate(matches):
-	    if m.distance < 0.7*n.distance:
-	        count += 1
+		if m.distance < 0.7*n.distance:
+			count += 1
 	
 	if count > min_matches:
 		return True
@@ -99,13 +118,20 @@ def check_sort_directory(key,value):
 		if has_files:
 			parser.error("--{} must be an empty directory".format(key))
 
-def valid_image(imgfile_name):
-	valid_endings = [".jpg",".png"]
+def valid_ending(filename,valid_ending_list):
 	valid = False
-	for ending in valid_endings:
-		if ending in imgfile_name:
+	for ending in valid_ending_list:
+		if ending in filename:
 			return True
 	return False
+
+def valid_image(imgfile_name):
+	valid_endings = [".jpg",".png"]
+	return valid_ending(imgfile_name, valid_endings)
+
+def valid_text(textfine_name):
+	valid_endings = [".txt"]
+	return valid_ending(textfine_name, valid_endings)
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--key_dir", type=str, help="directory where keys are located")
@@ -125,12 +151,20 @@ if __name__ == "__main__":
 	
 	#Generate a list of keys to search for
 	key_img_dict = dict()
+	key_text_dict = dict()
 	for i, (dirpath, dirnames, files) in enumerate(os.walk(args.key_dir)):
 		if i > 0:
-			valid_files = [f for f in files if valid_image(f)]
-			valid_imgs = [cv2.imread(dirpath+"/"+f,0) for f in valid_files]
+			valid_img_files = [f for f in files if valid_image(f)]
+			valid_text_files = [f for f in files if valid_text(f)]
+			valid_imgs = [cv2.imread(dirpath+"/"+f,0) for f in valid_img_files]
 			key_name = dirpath.split(os.path.sep)[-1]
 			key_img_dict[key_name] = valid_imgs
+			key_texts = []
+			for text_file in valid_text_files:
+				f = open(dirpath+os.path.sep+text_file,"r")
+				key_texts += [line.lower().replace("\n","") for line in f.readlines()]
+			key_text_dict[key_name] = key_texts
+
 			#Create sorted key folders
 			if not os.path.isdir(args.sort_dir+os.path.sep+key_name):
 				os.mkdir(args.sort_dir+os.path.sep+key_name)
@@ -139,6 +173,7 @@ if __name__ == "__main__":
 		os.mkdir(args.sort_dir+os.path.sep+"unsorted")
 	
 
+	print key_text_dict
 	#Generate a list of documents
 	documents = glob.glob(args.doc_dir+os.path.sep+"*.pdf")
 
@@ -153,6 +188,7 @@ if __name__ == "__main__":
 		for key_name,key_images in key_img_dict.iteritems():
 			for key_image in key_images:
 				for doc_image in images:
+
 					if detectKeyInImage(key_image, doc_image):
 						print "{} is classified as {}".format(doc,key_name)
 						doc_name = doc.split(os.path.sep)[-1]
@@ -169,19 +205,11 @@ if __name__ == "__main__":
 
 		if not classified:
 			print "{} is unclassified".format(doc)
+			doc_name = doc.split(os.path.sep)[-1]
 			new_loc = args.sort_dir+os.path.sep+"unsorted"+os.path.sep+doc_name
 			shutil.copyfile(doc,new_loc)
 
-	#TO DO
-	
 
-	#Iterate Trough Keys
-	#Iterate Through Key Images
-	#Iterate Through Documents
-	#Iterate Through Document Images
-	#If Key Image/Doc Image Match Move to sorted folder
-
-	#Done
 
 
 
